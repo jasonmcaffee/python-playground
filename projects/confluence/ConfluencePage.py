d9808a752662560b0d27e54f85f764c82a7585b1
jmcaffee <jmcaffee@sofi.org>
2023-04-05 15:21:28 -0600

    recursively get child pages under a page wip.

---
+++ projects/confluence/ConfluencePage.py
@@ -0,0 +1,6 @@
+class ConfluencePage:
+    def __init__(self, html_value: str, page_id: str, title: str, web_url: str):
+        self.html_value = html_value
+        self.page_id = page_id
+        self.title = title
+        self.web_url = web_url
--- projects/confluence/services/ConfluenceAPI.py
+++ projects/confluence/services/ConfluenceAPI.py
@@ -1,12 +1,16 @@
 import os
+from typing import Any, Optional
 from atlassian import Confluence
 from bs4 import BeautifulSoup
+from operator import itemgetter
+from projects.confluence.ConfluencePage import ConfluencePage
 
 
 # Singleton wrapper/helper for confluence api
 # https://python-patterns.guide/gang-of-four/singleton/
 # https://atlassian-python-api.readthedocs.io/
 # https://atlassian-python-api.readthedocs.io/confluence.html
+# https://docs.atlassian.com/ConfluenceServer/rest/8.2.0/#api/content-getContentById
 # BeautifulSoup for converting raw html, returned by confluence, to text.
 # It also provides a query-esque api for finding elements
 # https://www.geeksforgeeks.org/converting-html-to-text-with-beautifulsoup/
@@ -29,24 +33,49 @@ class ConfluenceAPI:
         api_token = os.getenv('CONFLUENCE_API_TOKEN')
         self.confluence = Confluence(url=base_url, username=username, password=api_token)
 
-    def does_page_exist(self, page_title: str = 'Core Services'):
-        return self.confluence.page_exists(space=self._confluence_space, title=page_title)
-
     def get_pages_from_space(self, start=10, limit=20):
         pages = self.confluence.get_all_pages_from_space(space=self._confluence_space, start=start, limit=limit)
         for page in pages:
             print('=========================')
             print(page)
 
+    # wip: get page content and child pages
     def get_page_by_title(self, title: str):
+        # get the page and its html content by expanding body.storage
         page = self.confluence.get_page_by_title(space=self._confluence_space, title=title, expand="body.storage")
-        html_value = page.get('body', {}).get('storage', {}).get('value', None)
-        page_soup = BeautifulSoup(html_value, "html.parser")
-        page_as_text = page_soup.get_text()
-        # print(f'page is ${page}')
-        print(f'page content is {page_as_text}')
-        print(f'page html is {html_value}')
+        confluence_page = create_confluence_page_from_page_data(page)
+        print(f'page by title: title: {confluence_page.title}, web_url: {confluence_page.web_url}, page_id: {confluence_page.page_id}')
+        self.get_all_child_pages(confluence_page.page_id)
+
+    # wip: get page children data.
+    # todo: iterate over all children, recursively
+    def get_all_child_pages(self, page_id: str, start=0, limit=2, current_depth=0, max_depth=2):
+        child_pages = self.confluence.get_page_child_by_type(page_id, type="page", start=start, limit=limit, expand="body.storage")
+        for child in child_pages:
+            child_confluence_page = create_confluence_page_from_page_data(child)
+            print(f'child page title: {child_confluence_page.title}')
+
+        current_depth += 1
+        if (len(child_pages) > 0) and current_depth <= max_depth:
+            print(f'getting more child pages')
+            new_start = start + limit
+            self.get_all_child_pages(page_id=page_id, start=new_start, current_depth=current_depth, max_depth=max_depth)
 
     # test function, delete later
     def do_stuff(self):
         self.get_page_by_title(title='Core Services')
+
+
+# Get a strongly typed instance with the data we care about
+def create_confluence_page_from_page_data(page: Optional[Any]):
+    page_id, title, links = itemgetter('id', 'title', '_links')(page)
+    html_value = page.get('body', {}).get('storage', {}).get('value', None)
+    web_url = links.get('webui', None)
+    # print(f'page_id: {page_id}, title: {title}, web_url: {web_url},')
+    # print(f'html_value: {html_value}')
+    # # convert the html to text
+    # page_soup = BeautifulSoup(html_value, "html.parser")
+    # page_as_text = page_soup.get_text()
+    # # print(f'page is {page}')
+    # print(f'page content is {page_as_text}')
+    return ConfluencePage(page_id=page_id, title=title, web_url=web_url, html_value=html_value)
