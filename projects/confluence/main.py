06e6d4737707d350521be7f8782a3f0d5fa25840
jmcaffee <jmcaffee@sofi.org>
2023-04-09 19:03:00 -0600

    documentation

--- projects/confluence/main.py
+++ projects/confluence/main.py
@@ -4,7 +4,7 @@ from projects.confluence.services.ConfluenceAPI import ConfluenceAPI
 def main():
     print('retrieving confluence pages and saving them to the db...')
     confluence = ConfluenceAPI()
-    confluence.get_confluence_data_and_save_to_db(title='SoFi Engineering Blog')
+    confluence.get_confluence_data_and_save_to_db(title='Contacts')
     print('process complete! All pages saved to the db!')
 
 
