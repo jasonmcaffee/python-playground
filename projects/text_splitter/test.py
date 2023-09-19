cf038105c58c4c021915c126560f8b0a775b0bef
jmcaffee <jmcaffee@sofi.org>
2023-09-19 04:31:50 -0600

    grouping by token length

--- projects/text_splitter/test.py
+++ projects/text_splitter/test.py
@@ -1,3 +1,4 @@
+import math
 # get an overlap of text, but split only at spaces inbetween words.
 # "hello I am jason and I am on an airplane"
 # should become
@@ -37,15 +38,119 @@ def split_string_at_nearest_space(string_to_split, end_position):
     start_text = string_to_split[0: highest_index_to_split_so_word_isnt_cut]
     return start_text, end_text
 
+# broken into groups of 20 with overlap amount of 10
+# I am the walrus that
+#  lives in the
+# basement and drives
+# a blue car that
+# everyone loves.
+#
+# I am the walrus that
+# walrus that lives in
+# lives
+#
+def break_into_groups_of_N_with_overlaps_of_Y(text, group_length, overlap_length):
+    groups = []
+    current_start_index = 0
+    next_end_index_to_split_so_word_isnt_cut = find_index_word_split_before_max_length(text, end_index=group_length)
+    # prevent unlikely scenario where word length is greater than group_length
+    previous_next_end_index_to_split_so_word_isnt_cut = -1
+    print(f"text length: {len(text)}")
+    count = 0
+    while(count < 10):
+        print("=================================")
+        count = count + 1
 
-# I am the walrus that lives in the basement
-# broken into groups of 3
-# 1. I am the
-# 2. walrus that lives
-# 3. in the basement.
+        group = text[current_start_index: next_end_index_to_split_so_word_isnt_cut]
+        groups.append(group)
+        print(f"current_start_index: {current_start_index} next_end_index_to_split_so_word_isnt_cut: {next_end_index_to_split_so_word_isnt_cut}")
+        print(f"group:-{group}- length: {len(group)}")
 
+        # if(next_end_index_to_split_so_word_isnt_cut + group_length < len(text)):
+        #     next_end_index_to_split_so_word_isnt_cut = len(text)
+
+        current_start_index = next_end_index_to_split_so_word_isnt_cut
+        #current_start_index should now have half group_length added
+        # current_start_index = math.floor(current_start_index + (group_length - overlap_length))
+        # current_start_index = text.find(" ", current_start_index)
+        # current_start_index = next_end_index_to_split_so_word_isnt_cut
+        next_end_index_to_split_so_word_isnt_cut = find_index_word_split_before_max_length(text, start_index=current_start_index, end_index=current_start_index + group_length)
+
+        #ensure last word is captured
+        if(current_start_index == next_end_index_to_split_so_word_isnt_cut):
+            next_end_index_to_split_so_word_isnt_cut = len(text)
+
+        # overlap_end_index = find_index_word_split_before_max_length(text, next_end_index_to_split_so_word_isnt_cut, group_length + next_end_index_to_split_so_word_isnt_cut,)
+        # print(f"overlap_end_index: {overlap_end_index}")
+        # print(f"next_end_index_to_split_so_word_isnt_cut: {next_end_index_to_split_so_word_isnt_cut}")
+        # overlap_group = text[next_end_index_to_split_so_word_isnt_cut: overlap_end_index ]
+        # print(f"overlap group:-{overlap_group}-")
+
+        # return
+    return groups
+
+
+def find_index_word_split_before_max_length(text, start_index = 0, end_index=0):
+    """
+    text: I am the walrus that lives in the basement and drives a blue car that everyone loves.
+    max_length: 7
+    results in: 4
+    """
+    index = text.rfind(" ", start_index, end_index + 1) # +1
+    #include the " "
+    # if index < end_index:
+    #     index += 1
+    # print(f"index is {index}")
+    # print(f"word after index:-{text[index: len(text)]}-")
+    # print(f"word before index:-{text[0: index]}-")
+    return index
+
+# 1 token == 4 characters
+def easy_grouping(text, group_token_length):
+    """Note: These must be joined back together with ' '.join(groups) """
+    groups = []
+    current_group = []
+    current_group_token_count = 0
+    words = text.split(" ")
+    while len(words) > 0:
+        word = words[0]
+        current_word_token_count = calculate_tokens_for_word(word + " ")
+        if(current_group_token_count + current_word_token_count <= group_token_length):
+            words.pop(0)
+            current_group.append(word)
+            current_group_token_count += current_word_token_count
+            #if the we are out of words and less than the group_token_length, we need to get out.
+            if(len(words)) == 0:
+                current_group_text = " ".join(current_group)
+                groups.append(current_group_text)
+        else:
+            current_group_text = " ".join(current_group)
+            groups.append(current_group_text)
+            current_group = []
+            current_group_token_count = 0
+    return groups
+
+def calculate_tokens_for_word(word):
+    return math.ceil(len(word) / 4)
 
 if __name__ == "__main__":
-    start_text, end_text = split_string_at_nearest_space("I am the walrus that lives in the basement", 2)
-    print(f"result: {start_text},{end_text}")
-    # main()
+    text = " I am the walrus that lives in the basement and drives a blue car that everyone loves. "
+    groups = easy_grouping(text, 10)
+    print(groups)
+    final = " ".join(groups)
+    print(final == text)
+    # groups = text.split(" ")
+    # print(groups)
+    # result = " ".join(groups)
+    # print(result)
+    # print(result == text)
+    # find_index_word_split_before_max_length(text, end_index=2)
+    # start_text, end_text = split_string_at_nearest_space("I am the walrus that lives in the basement", 2)
+    # print(f"result: {start_text},{end_text}")
+    #main()
+    # groups = break_into_groups_of_N_with_overlaps_of_Y(text, 40, 0)
+    # print(f"groups are {groups}")
+    # sub = text[0:5]
+    # sub = sub[2:3]
+    # print(sub)
+    # print("I"[1])
