7b24d934342499f5f108de06810ad36c655693ef
Jason McAffee <jasonlmcaffee@gmail.com>
2023-12-09 16:26:02 -0700

    sql query for transactions working

--- projects/chatgpt/services/llm_callable_functions/TransactionQueries.py
+++ projects/chatgpt/services/llm_callable_functions/TransactionQueries.py
@@ -1,13 +1,25 @@
 import json
 import textwrap
+import psycopg2
+import csv
+import io
 
 from projects.chatgpt.decorators.chatgpt_tool_data import chatgpt_tool_data
 from projects.chatgpt.services.llm_callable_functions.CallableFunctionServiceBase import CallableFunctionServiceBase
 
+conn_params = {
+    'dbname': 'postgres',
+    'user': 'postgres',
+    'password': 'postgrespw',
+    'host': 'localhost',
+    'port': '55000',
+    'options': '-c search_path=financial_data',
+}
 
 class TransactionQueries(CallableFunctionServiceBase):
     def __init__(self):
         super().__init__()
+        self.connection = psycopg2.connect(**conn_params)
 
     @chatgpt_tool_data({
         "type": "function",
@@ -15,7 +27,10 @@ class TransactionQueries(CallableFunctionServiceBase):
             "name": "transaction_query",
             "description": textwrap.dedent("""Function to answer questions related to a persons financial spending habits, accounts, balances, and transactions.
             Uses the passed in postgres sql query to retrieve financial transaction data, such as transactions between a certain date, accounts, transactions for a certain category, etc.
-            Return value will be the result of the sql query as a csv string.
+            When a category is searched for, text case should be ignored, and the bot should understand that some transactions will have variances on the same category. e.g. 'Food' might also be 'Dining', so the bot should find perform semantic searches for categories with similar meaning.
+            When an account type is searched for, text case should be ignored, and the bot should perform semantic search. .e.g. 'Credit' is the same as 'credit card'
+            Return value will be the result of the sql query as a csv string, or an exception message from psycopg2.  
+            If the bot receives an exception, it should try another sql query for a maximum of 5 times.
             """)
             ,
             "parameters": {
@@ -24,7 +39,9 @@ class TransactionQueries(CallableFunctionServiceBase):
                     "postgres_sql_query": {
                         "type": "string",
                         "description": textwrap.dedent(
-                            """The generated postgres sql query, created by the bot, which queries the tables described in the description of this function.
+                            """The generated postgres sql query, created by the bot, which queries the tables described below.
+                            All queries should ignore text case when matching category and account_type.
+                            The bot should use join statements when necessary.
                             The tables available are: 
                             CREATE TABLE  if not exists Account ( # Table for storing account details
                                 id VARCHAR(255) PRIMARY KEY,
@@ -49,5 +66,36 @@ class TransactionQueries(CallableFunctionServiceBase):
     })
     def transaction_query(self, args: str):
         arguments = json.loads(args)
-        print(f"transactions_query called with args {arguments}")
+        # print(f"transactions_query called with args {arguments}")
         postgres_sql_query = arguments['postgres_sql_query']
+        csv_results = self.query_to_csv_string(self.connection, postgres_sql_query)
+        print(f"query: {postgres_sql_query}")
+        print(f"result: {csv_results}")
+        return csv_results
+
+    def query_to_csv_string(self, connection, query):
+        try:
+            # Cursor for database connection
+            cursor = connection.cursor()
+            # Execute the query
+            cursor.execute(query)
+            # Fetch the results
+            results = cursor.fetchall()
+            # Column names from the cursor
+            col_names = [desc[0] for desc in cursor.description]
+            # Create a CSV output from the results
+            output = io.StringIO()
+            csv_writer = csv.writer(output)
+            # Write the header
+            csv_writer.writerow(col_names)
+            # Write the data
+            csv_writer.writerows(results)
+            # Get the CSV string
+            csv_string = output.getvalue()
+            output.close()
+            return csv_string
+        except Exception as e:
+            return str(e)
+        finally:
+            if 'cursor' in locals():
+                cursor.close()
