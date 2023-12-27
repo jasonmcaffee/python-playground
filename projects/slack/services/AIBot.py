2665b44a383b5d56a42d847c145ece2abf139e81
Jason McAffee <jasonlmcaffee@gmail.com>
2023-12-27 13:22:37 -0700

    command handling for summarize-url working.

--- projects/slack/services/AIBot.py
+++ projects/slack/services/AIBot.py
@@ -1,9 +1,11 @@
 import threading
 import os
 import re
+import requests
 from dotenv import load_dotenv
 from slack_sdk.rtm_v2 import RTMClient
 from projects.slack.services.SimpleChatGPT import SimpleChatGPT
+from bs4 import BeautifulSoup
 
 load_dotenv()
 slack_token = os.getenv('SLACK_API_TOKEN')
@@ -48,13 +50,10 @@ class AIBot:
         :return:
         """
         message_received = event.get('text', '')
+        # print(event)
         if self.is_message_addressed_to_ai_bot(message_received):
-            print('ai_bot_user')
-
-            thread = threading.Thread(
-                target=self.handle_ai_bot_message,
-                args=(client, event)
-            )
+            # start a new thread to handle the message
+            thread = threading.Thread(target=self.handle_ai_bot_message, args=(client, event))
             thread.start()
             # handle_ai_bot_message(client, event)
 
@@ -76,23 +75,33 @@ class AIBot:
 
         # parse the prompt the user has sent AI by removing the @AI from the prompt.
         prompt = message_received.replace(self.ai_bot_user, '')
+        prompt = prompt.lstrip()  # strip any leading whitespace
 
         # Initial message sent to the channel
         initial_response = client.web_client.chat_postMessage(channel=channel_id, text="...", thread_ts=thread_ts)
-
         # Retrieve timestamp of the initial response, which is how we uniquely identify the message, so
         initial_response_ts = initial_response['ts']
+
+        # check to see if there are any commands in the message, and modify the prompt.  e.g. summarize-url will fetch
+        # the url then send it to the LLM
+        try:
+            prompt = self.modify_prompt_based_on_command(prompt)
+        except Exception as e:
+            message = f"There was an issue with your request: {e}"
+            client.web_client.chat_update(channel=channel_id, ts=initial_response_ts, text=message, thread_ts=thread_ts)
+            return
 
         # we will get a stream of text chunks from the LLM, which we will append to cumulative text.
         # we will edit the slack message
         cumulative_text = ""
-        last_update_length = len(cumulative_text) # how long cumulative was the last time we performed an update.
+        last_update_length = len(cumulative_text)  # how long cumulative was the last time we performed an update.
         edit_slack_message_after_n_chars_received_from_llm = 20
 
         def handle_response_completed():
             nonlocal cumulative_text, last_update_length
             sanitized_cumulative_text = self.sanitize_output(cumulative_text)
-            client.web_client.chat_update(channel=channel_id, ts=initial_response_ts, text=sanitized_cumulative_text, thread_ts=thread_ts)
+            client.web_client.chat_update(channel=channel_id, ts=initial_response_ts, text=sanitized_cumulative_text,
+                                          thread_ts=thread_ts)
             last_update_length = len(cumulative_text)
 
         def handle_text_received(text):
@@ -109,7 +118,8 @@ class AIBot:
             # if is_response_completed or has_received_enough_text_from_llm:
             if has_received_enough_text_from_llm:
                 sanitized_cumulative_text = self.sanitize_output(cumulative_text)
-                client.web_client.chat_update(channel=channel_id, ts=initial_response_ts, text=sanitized_cumulative_text, thread_ts=thread_ts)
+                client.web_client.chat_update(channel=channel_id, ts=initial_response_ts,
+                                              text=sanitized_cumulative_text, thread_ts=thread_ts)
                 last_update_length = len(cumulative_text)
 
         self.llm_prompt(prompt, handle_text_received, handle_response_completed)
@@ -135,3 +145,74 @@ class AIBot:
         # Replace Slack user mention patterns (like <@UEU96U15F>) with the user ID in backticks
         escaped_message = re.sub(r'<@(U[A-Z0-9]+)>', r'`\1`', text)
         return escaped_message
+
+    def modify_prompt_based_on_command(self, prompt):
+        """
+        certain prompt contains commands
+        :param prompt:
+        :return:
+        """
+
+        potential_summarize_url_command = self.parse_summarize_url_command(prompt)
+        if potential_summarize_url_command is not None:
+            prompt = self.modify_prompt_to_summarize_url(potential_summarize_url_command)
+
+        return prompt
+
+    def modify_prompt_to_summarize_url(self, potential_summarize_url_command):
+        new_message_with_command_stripped, slash_command, url = potential_summarize_url_command
+        html_content = self.get_html_content(url)
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        # Extract text
+        main_content = soup.find('main') or soup.find('article') or soup.find('div', {'id': 'content'}) or soup.find(
+            'div', class_=lambda x: x and 'content' in x)
+
+        # Extract text from the main content if found
+        if main_content:
+            website_as_text = main_content.get_text(separator='\n', strip=True)
+        else:
+            website_as_text = soup.get_text(separator='\n', strip=True)
+            print("Main content not found.")
+
+        partial_website_as_text = website_as_text[0:2000]  # avoid going over our token limit.
+
+        new_prompt = f"This text is from a website where the html was parsed using BeautifulSoup. Please summarize this text into 300 words or less: \n {partial_website_as_text}"
+        return new_prompt
+
+    def parse_summarize_url_command(self, message):
+        """
+        Parse a message that starts with a command '/summarize-url' followed by a well-formatted URL.
+
+        :param message: str, the message to parse
+        :return: tuple, (new_message_with_command_stripped, slash_command, url)
+        :raises ValueError: if the URL is invalid
+        """
+        if not message.startswith('/summarize-url'):
+            return
+
+            # Regular expression to match the command pattern with a more inclusive URL pattern
+        pattern = r'^(\/summarize-url)\s+<([^>]+)>(.*)$'
+        match = re.match(pattern, message)
+
+        if match:
+            slash_command, url, new_message_with_command_stripped = match.groups()
+            return new_message_with_command_stripped, slash_command, url
+        else:
+            raise ValueError(f"Invalid message format or URL: {message}")
+
+    def get_html_content(self, url: str):
+        """
+            Retrieve the raw HTML content of a URL.
+
+            :param url: str, the URL to retrieve
+            :return: str, the raw HTML content
+            :raises Exception: if the response status code is not 2xx
+            """
+        response = requests.get(url)
+
+        # Check if the response status code is in the 2xx success range
+        if response.status_code // 100 == 2:
+            return response.text
+        else:
+            raise Exception(f"Failed to retrieve URL. Status code: {response.status_code}")
